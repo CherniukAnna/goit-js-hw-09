9e843cf971309d20532d9248bcf3f268bfac3499
CherniukAnna <chernukanna@gmail.com>
2023-01-30 19:47:04 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+const colorChanger = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      bodyColorChanger();
+    }, 1000);
+    this.startBtn.disabled = true;
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    startBtn.disabled = false;
+  },
+};
+startBtn.addEventListener('click', () => {
+  colorChanger.start();
+});
+stopBtn.addEventListener('click', () => {
+  colorChanger.stop();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bodyColorChanger() {
+  body.style.backgroundColor = getRandomHexColor();
+}
